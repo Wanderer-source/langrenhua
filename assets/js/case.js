f06fa465399e787af238c9140986c47761ca2d31
孙国华 <2103576329@qq.com>
2026-09-03 17:45:52 +0800

    feat: 产品子页面底部增加返回作品集按钮

--- assets/js/case.js
+++ assets/js/case.js
@@ -31,7 +31,8 @@
     }).join('') +
     '</nav>';
 
-  mount.innerHTML = '<a class="back-link" href="index.html">← 返回作品集</a>' + toc + res.html;
+  mount.innerHTML = '<a class="back-link" href="index.html">← 返回作品集</a>' + toc + res.html +
+    '<div class="case-foot"><a class="case-back-btn" href="index.html">← 返回作品集</a></div>';
   document.title = c.title + ' · 孙国华作品集';
   window.CaseApp.bindAll();
   initCaseTOC();
